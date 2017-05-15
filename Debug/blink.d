bad91922e8c0c03f5450b2ec7aa12d5d0a96adf4
awwheele <adrewwheeler@gmail.com>
2017-05-14 17:22:46 -0700

    Beginning
    First demo 5/14

---
+++ Debug/blink.d
@@ -0,0 +1,17 @@
+# FIXED
+
+blink.obj: ../blink.c
+blink.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430.h
+blink.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430fr5994.h
+blink.obj: C:/ti/ccsv7/ccs_base/msp430/include/in430.h
+blink.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics.h
+blink.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics_legacy_undefs.h
+blink.obj: C:/ti/ccsv7/ccs_base/msp430/include/legacy.h
+
+../blink.c: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430fr5994.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/legacy.h: 
---
+++ main.c
@@ -0,0 +1,128 @@
+
+
+//***************************************************************************************
+//  MSP430 Blink the LED Demo - Software Toggle P1.0
+//
+//  Description; Toggle P1.0 by xor'ing P1.0 inside of a software loop.
+//  ACLK = n/a, MCLK = SMCLK = default DCO
+//
+//                MSP430x5xx
+//             -----------------
+//         /|\|              XIN|-
+//          | |                 |
+//          --|RST          XOUT|-
+//            |                 |
+//            |             P1.0|-->LED
+//
+//  J. Stevenson
+//  Texas Instruments, Inc
+//  July 2013
+//  Built with Code Composer Studio v5
+//***************************************************************************************
+
+#include <msp430.h>
+#include "main.h"
+
+void initADC() {
+
+    P1SEL1 |= BIT5;                         // Configure P1.1 for ADC
+    P1SEL0 |= BIT5;
+
+    // Configure ADC12
+    ADC12CTL0 = ADC12SHT0_2 | ADC12ON;      // Sampling time, S&H=16, ADC12 on
+    ADC12CTL1 = ADC12SHP;                   // Use sampling timer
+    ADC12CTL2 |= ADC12RES_2;                // 12-bit conversion results
+    ADC12MCTL0 |= ADC12INCH_5;              // A1 ADC input select; Vref=AVCC
+    ADC12IER0 |= ADC12IE0;                  // Enable ADC conv complete interrupt
+
+}
+
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
+                                            // to activate previously configured port settings
+    P1DIR |= 0x01 | 0x02;                          // Set P1.0 to output direction
+
+    P4DIR |= TILE_CTRL;
+    P2DIR |= MAG_CTRL;
+
+    initADC();
+
+    //P1OUT &= ~0x02;
+    P1OUT |= 0x02 | BIT0;
+    P4OUT = TILE_0;                              //Select Tile 0
+    P2OUT = MAG_0;                              //Select Mag 0
+
+    for(;;) {
+
+        P1OUT ^= 0x02;
+        __delay_cycles(5000);
+        ADC12CTL0 |= ADC12ENC | ADC12SC;    // Start sampling/conversion
+
+    }
+
+    //return 0;
+}
+
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector = ADC12_B_VECTOR
+__interrupt void ADC12_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(ADC12_B_VECTOR))) ADC12_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+    switch(__even_in_range(ADC12IV, ADC12IV__ADC12RDYIFG))
+    {
+        case ADC12IV__NONE:        break;   // Vector  0:  No interrupt
+        case ADC12IV__ADC12OVIFG:  break;   // Vector  2:  ADC12MEMx Overflow
+        case ADC12IV__ADC12TOVIFG: break;   // Vector  4:  Conversion time overflow
+        case ADC12IV__ADC12HIIFG:  break;   // Vector  6:  ADC12BHI
+        case ADC12IV__ADC12LOIFG:  break;   // Vector  8:  ADC12BLO
+        case ADC12IV__ADC12INIFG:  break;   // Vector 10:  ADC12BIN
+        case ADC12IV__ADC12IFG0:            // Vector 12:  ADC12MEM0 Interrupt
+            if (ADC12MEM0 >= 0x7ff)         // ADC12MEM0 = A1 > 0.5AVcc?
+                P1OUT |= BIT0;              // P1.0 = 1
+            else
+                P1OUT &= ~BIT0;             // P1.0 = 0
+
+                // Exit from LPM0 and continue executing main
+                //__bic_SR_register_on_exit(LPM0_bits);
+            break;
+        case ADC12IV__ADC12IFG1:   break;   // Vector 14:  ADC12MEM1
+        case ADC12IV__ADC12IFG2:   break;   // Vector 16:  ADC12MEM2
+        case ADC12IV__ADC12IFG3:   break;   // Vector 18:  ADC12MEM3
+        case ADC12IV__ADC12IFG4:   break;   // Vector 20:  ADC12MEM4
+        case ADC12IV__ADC12IFG5:   break;   // Vector 22:  ADC12MEM5
+        case ADC12IV__ADC12IFG6:   break;   // Vector 24:  ADC12MEM6
+        case ADC12IV__ADC12IFG7:   break;   // Vector 26:  ADC12MEM7
+        case ADC12IV__ADC12IFG8:   break;   // Vector 28:  ADC12MEM8
+        case ADC12IV__ADC12IFG9:   break;   // Vector 30:  ADC12MEM9
+        case ADC12IV__ADC12IFG10:  break;   // Vector 32:  ADC12MEM10
+        case ADC12IV__ADC12IFG11:  break;   // Vector 34:  ADC12MEM11
+        case ADC12IV__ADC12IFG12:  break;   // Vector 36:  ADC12MEM12
+        case ADC12IV__ADC12IFG13:  break;   // Vector 38:  ADC12MEM13
+        case ADC12IV__ADC12IFG14:  break;   // Vector 40:  ADC12MEM14
+        case ADC12IV__ADC12IFG15:  break;   // Vector 42:  ADC12MEM15
+        case ADC12IV__ADC12IFG16:  break;   // Vector 44:  ADC12MEM16
+        case ADC12IV__ADC12IFG17:  break;   // Vector 46:  ADC12MEM17
+        case ADC12IV__ADC12IFG18:  break;   // Vector 48:  ADC12MEM18
+        case ADC12IV__ADC12IFG19:  break;   // Vector 50:  ADC12MEM19
+        case ADC12IV__ADC12IFG20:  break;   // Vector 52:  ADC12MEM20
+        case ADC12IV__ADC12IFG21:  break;   // Vector 54:  ADC12MEM21
+        case ADC12IV__ADC12IFG22:  break;   // Vector 56:  ADC12MEM22
+        case ADC12IV__ADC12IFG23:  break;   // Vector 58:  ADC12MEM23
+        case ADC12IV__ADC12IFG24:  break;   // Vector 60:  ADC12MEM24
+        case ADC12IV__ADC12IFG25:  break;   // Vector 62:  ADC12MEM25
+        case ADC12IV__ADC12IFG26:  break;   // Vector 64:  ADC12MEM26
+        case ADC12IV__ADC12IFG27:  break;   // Vector 66:  ADC12MEM27
+        case ADC12IV__ADC12IFG28:  break;   // Vector 68:  ADC12MEM28
+        case ADC12IV__ADC12IFG29:  break;   // Vector 70:  ADC12MEM29
+        case ADC12IV__ADC12IFG30:  break;   // Vector 72:  ADC12MEM30
+        case ADC12IV__ADC12IFG31:  break;   // Vector 74:  ADC12MEM31
+        case ADC12IV__ADC12RDYIFG: break;   // Vector 76:  ADC12RDY
+        default: break;
+    }
+}
+
