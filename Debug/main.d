368e857e5a4c09eeffef9440a2efec23e9a1a08a
twlennertz <twlennertz@gmail.com>
2017-06-01 11:15:22 -0700

    agh

---
+++ Debug/main.d
@@ -0,0 +1,31 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430fr5994.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccsv7/ccs_base/msp430/include/legacy.h
+main.obj: ../main.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdio.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/linkage.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdarg.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdint.h
+main.obj: ../tiles.h
+main.obj: ../graph.h
+
+../main.c: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430fr5994.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/legacy.h: 
+../main.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdint.h: 
+../tiles.h: 
+../graph.h: 
---
+++ Debug/tiles.d
@@ -0,0 +1,29 @@
+# FIXED
+
+tiles.obj: ../tiles.c
+tiles.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430.h
+tiles.obj: C:/ti/ccsv7/ccs_base/msp430/include/msp430fr5994.h
+tiles.obj: C:/ti/ccsv7/ccs_base/msp430/include/in430.h
+tiles.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics.h
+tiles.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics_legacy_undefs.h
+tiles.obj: C:/ti/ccsv7/ccs_base/msp430/include/legacy.h
+tiles.obj: ../main.h
+tiles.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdio.h
+tiles.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/linkage.h
+tiles.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdarg.h
+tiles.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdint.h
+tiles.obj: ../tiles.h
+
+../tiles.c: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/msp430fr5994.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv7/ccs_base/msp430/include/legacy.h: 
+../main.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-msp430_16.9.1.LTS/include/stdint.h: 
+../tiles.h: 
